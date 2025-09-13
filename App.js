feb1222dcf1641015373973244660c20c77e2b2a
dineshpardhu <149290510+dineshpardhu@users.noreply.github.com>
2025-09-13 15:41:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚀 Hello from React on S3!</h1>
+      <p>This site is deployed using Jenkins + S3</p>
+    </div>
+  );
+}
+
+export default App;
